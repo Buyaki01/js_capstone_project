7b287855fa570146e5b0a21a9365bcd2703b3c0c
Buyaki <rittahbuyaki@gmail.com>
2021-12-22 18:46:17 +0300

    Add changes to the comments functionality

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,18 +4,23 @@ import '../scss/style.scss';
 import { showsList } from './Display/cards.js';
 
 showsList.renderCards();
-const requestURLComments = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/vBqRZTU315QO9ChybbKR/comments/';
+const postURLComments = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/o7hamWo6ePWlkw5D7zAB/comments';
+const getURLComments = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/o7hamWo6ePWlkw5D7zAB/comments?item_id=item1'; 
 const getComments = async () => {
-  const data = await fetch(requestURLComments);
+  const data = await fetch(getURLComments);
   const response = await data.json();
   const comments = await response.result;
-  return comments;
+  console.log(comments);
 };
 
-const addComment = async (user, comment) => {
-  fetch(requestURLComments, {
+const addComment = async (getName, getComment) => {
+  fetch(postURLComments, {
     method: 'POST',
-    body: JSON.stringify({ user, comment }),
+    body: JSON.stringify({
+      item_id: 'item1',
+      username: getName,
+      comment: getComment, 
+    }),
     mode: 'cors',
     headers: { 'Content-type': 'application/json' },
   });
@@ -26,9 +31,28 @@ inputForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const getName = document.querySelector('#exampleFormControlInput1').value;
   const getComment = document.querySelector('#exampleFormControlTextarea1').value;
+  console.log(getName);
+  console.log(getComment);
   addComment(getName, getComment);
   inputForm.reset();
 });
 
+const displayComment = document.querySelector('.display-comment');
+const commentElement = (getName, getComment) => {
+  const commentContainer = document.createElement('div');
+  const commentData = document.createElement('p');
+
+  commentData.innerHTML = `${getName}: ${getComment} `;
+
+  commentContainer.appendChild(commentData);
+  displayComment.appendChild(commentContainer);
+};
+ 
+const displayAllComments = async () => {
+  const allComments = await getComments();
+  allComments.forEach((comment) => commentElement(comment.getName, comment.getComment));
+};
+
 const viewCommentsBtn = document.querySelector('.display-comment-btn');
-viewCommentsBtn.addEventListener('click', getComments());
+viewCommentsBtn.addEventListener('click', displayAllComments);
+displayAllComments();
