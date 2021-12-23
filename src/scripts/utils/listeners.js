dbf31ab89c53c7ceb30f9ef224cd465f70207ca1
vic778 <victoiremmanuelbarh@gmail.com>
2021-12-23 08:55:48 +0200

    Added function to shwo the number of likes

---
+++ src/scripts/utils/listeners.js
@@ -0,0 +1,19 @@
+import { involvement } from '../API/Involvement.js';
+
+class Listeners {
+  addLikeListener = (event) => {
+    event.addEventListener('click', () => {
+      involvement.setLike(event.dataset.id);
+      const likesToUpdate = document.querySelector(`.movie-${event.dataset.id}`);
+      involvement.getLikesOf(event.dataset.id)
+        .then((likes) => {
+          likesToUpdate.innerHTML = `${likes} people like this`;
+        });
+    });
+  }
+}
+
+const elisteners = new Listeners();
+
+// eslint-disable-next-line import/prefer-default-export
+export { elisteners };
